a6dadc328385d7a000e4b7430d6b478894454e3d
Harry Lincoln <harry.lincoln@edcoms.co.uk>
2017-01-16 14:40:54 +0000

    update with prep for faced and sharp for facial recognition

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # placegosling
 
 1. npm install
-2. change to your twitter api credentials in index.js
+2. add a env.js file in the root directory and enter API credentials:
+
+`process.env['TWITTER_CONSUMER_KEY'] = "";
+process.env['TWITTER_CONSUMER_SECRET'] = "";
+process.env['TWITTER_ACCESS_TOKEN'] = "";
+process.env['TWITTER_TOKEN_SECRET'] = "";`
+
+NOTE: you get these from creating an app in twitter:
+https://apps.twitter.com/app/new
+
 3. node index.js
--- index.js
+++ index.js
@@ -1,51 +1,74 @@
 var Twitter = require('twitter');
 var request = require('request');
 var Q = require('q');
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
+var childProc = require('child_process');
+// var sharp = require('sharp');
+var Faced = require('faced');
+var faced = new Faced();
+require('./env.js');
 
-var client = new Twitter({
-  consumer_key: "",
-  consumer_secret: "",
-  access_token_key: "",
-  access_token_secret: ""
+// facial recognition with cmake, make (GCC), pkg-config
+faced.detect('simon.jpg', function (faces, image, file) {
+  if (!faces) {
+    return console.log("No faces found!");
+  }
+
+  var face = faces[0];
+
+  console.log(
+    "Found a face at %d,%d with dimensions %dx%d",
+    face.getX(),
+    face.getY(),
+    face.getWidth(),
+    face.getHeight()
+  );
 });
 
-var str = '';
-function doTwitterParse() {
-  var deferred = Q.defer();
-  client.get('search/tweets', {q: 'ryangosling', count: 10}, function(error, data, response) {
-    if (!error) {
-      // console.log(data.statuses);
-      var statuses = data.statuses;
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_TOKEN_SECRET
+// });
 
-      for (var i = 0; i < statuses.length; i++) {
-        var words = statuses[i].text.split(' ');
+// var str = '';
+// function doTwitterParse() {
+//   var deferred = Q.defer();
+//   client.get('search/tweets', {q: 'ryan%20gosling', count: 10}, function(error, data, response) {
+//     if (!error) {
+//       // console.log(data.statuses);
+//       var statuses = data.statuses;
 
-        for (var j = 0; j < words.length; j++) {
-          var value = words[j];
+//       for (var i = 0; i < statuses.length; i++) {
+//         var words = statuses[i].text.split(' ');
+
+//         for (var j = 0; j < words.length; j++) {
+//           var value = words[j];
           
-          if (value.includes('https://t.co')) {
-            console.log('string getting saved --> ', value);
-            str = value;
-            deferred.resolve();
-            return true;
-          }
-        }
-      }
-    } else {
-      console.log(error);
-    }
-  });
-  return deferred.promise;
-}
-doTwitterParse().then(function() {
-  console.log('promise returned and here is the string -->', str);
-  request(str, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var htmlBod = body;
-      var $ = cheerio.load(htmlBod);
-      console.log('found it!', $('meta[property="og:image"]').attr('content'));
-    }
-  });
-});
+//           if (value.includes('https://t.co')) {
+//             console.log('string getting saved --> ', value);
+//             str = value;
+//             deferred.resolve();
+//             return true;
+//           }
+//         }
+//       }
+//     } else {
+//       console.log(error);
+//     }
+//   });
+//   return deferred.promise;
+// }
+// doTwitterParse().then(function() {
+//   console.log('promise returned and here is the string -->', str);
+//   request(str, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var htmlBod = body;
+//       var $ = cheerio.load(htmlBod);
+//       var img = $('meta[property="og:image"]').attr('content');
+//       console.log('found it!', $('meta[property="og:image"]').attr('content'));
+//     }
+//   });
+// });
 
